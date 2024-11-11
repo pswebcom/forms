a2e481e0cf60d6a117666c0645944ec302aafade
gugu <psdevco@gmail.com>
2024-11-10 22:08:03 -0500

    form html

--- index.php
+++ index.php
@@ -1,3 +1,31 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-echo "php";
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <form action="" method="post">
+        <div>Name:</div>
+        <div>
+            <input type="text" name="name" autocapitalize="off">
+        </div>
+
+
+        <div>Email:</div>
+        <div>
+            <input type="email" name="email" autocapitalize="off">
+        </div>
+
+        <div>
+            <input type="submit" value="Submit" name="form1">
+        </div>
+    </form>
+
+</body>
+
+</html>
